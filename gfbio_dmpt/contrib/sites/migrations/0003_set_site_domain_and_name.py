13a01ce75d0700513d98c031f595a4b219eb67cf
marc.weber01 <maweber@mpi-bremen.de>
2021-05-10 13:39:11 +0200

    864 fix dns name in constants

--- gfbio_dmpt/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ gfbio_dmpt/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "rdmo.gfbio.dev",
+            "domain": "dmpt.gfbio.dev",
             "name": "GFBio DMPT",
         },
     )
