de014951573d9d3b003bb2b1291b6f59b7ecdce7
marc.weber01 <maweber@mpi-bremen.de>
2021-07-20 22:01:09 +0200

    2 set up dmpt react app in django with simple welcome message
    - add Welcome componment
    - adapt urlPrefix and Route
    - de-comment dev html deps
    - add TemplateView for react app template
    - add url
    - add current build to django static files

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/local_quick_collect.sh
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/local_quick_collect.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 npm run build
-cp build/js/main.*.js ../../static/js/<APP_NAME>/main.<APP_NAME>.js
-cp build/js/main.*.js.map ../../static/js/<APP_NAME>/main.<APP_NAME>.js.map
+#cp build/js/main.*.js ../../static/js/gfbio_dmpt_form/main.dmp_app.js
+#cp build/js/main.*.js.map ../../static/js/gfbio_dmpt_form/main.dmp_app.js.map
+cp build/js/main.*.js ../../static/js/gfbio_dmpt_form/
+cp build/js/main.*.js.map ../../static/js/gfbio_dmpt_form/
 echo yes | docker-compose -f ../../../local.yml run --rm django python manage.py collectstatic
---
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/Welcome/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// TODO: admin like filters -> dedicated views
+// TODO: pagination -> via DRF
+// TODO: column sorting. data, type etc. -> dedicated view with triggers ?
+// TODO: admin style search ? how does admin know what field to search ?
+export default function Welcome() {
+
+    return (
+        <div>
+            <h1>Welcome to GFBio DMPT</h1>
+        </div>
+    );
+};
