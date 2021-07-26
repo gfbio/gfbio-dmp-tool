1cb5ca7e0a60f26a0aeb78ec31370b88509ffa17
marc.weber01 <maweber@mpi-bremen.de>
2021-07-26 15:27:37 +0200

    2 set up basic global context for rdmo data
    - comment automatic project creation

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/App.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line no-unused-vars
 import { Route, Switch } from 'react-router-dom';
 import Welcome from './Welcome';
 import Catalogs from './Catalogs';
 import DmptStart from './DmptStart';
+import RdmoContext from './RdmoContext';
 
 // TODO: to work when served in django template this prefix has to match
 //  the urls.py + global urls.py entry -> currently (...) regex=r'submissions/ui/', (...)
@@ -18,16 +19,47 @@ import DmptStart from './DmptStart';
 // eslint-disable-next-line no-unused-vars
 const urlPrefix = '/';
 
-const App = () => (
-    <Switch>
-        <Route exact path={`${urlPrefix}`} component={Welcome} />
-        <Route path={`${urlPrefix}catalogs`} component={Catalogs} />
-        <Route path={`${urlPrefix}start`} component={DmptStart} />
-        {/* <Route path={`${urlPrefix}:brokerSubmissionId/`} */}
-        {/*    component={SubmissionDetail}/> */}
-        {/* <Route path={`${urlPrefix}:brokerSubmissionId/`} */}
-        {/*    component={DetailBoard}/> */}
-    </Switch>
-);
+const App = () => {
+
+    // https://www.savaslabs.com/blog/using-react-global-state-hooks-and-context
+    const [sections, setSections] = useState({});
+    const [questionSets, setQuestionSets] = useState({});
+    const [questions, setQuestions] = useState({});
+
+    const assignSections = (data) => {
+        setSections(data);
+    };
+
+    const assignQuestionSets = (data) => {
+        setQuestionSets(data);
+    };
+
+    const assignQuestions = (data) => {
+        setQuestions(data);
+    };
+
+    const rdmoContext = {
+        section_data: sections,
+        question_set_data: questionSets,
+        questions_data: questions,
+        assignSections,
+        assignQuestionSets,
+        assignQuestions
+    };
+
+    return (
+        <RdmoContext.Provider value={rdmoContext}>
+            <Switch>
+                <Route exact path={`${urlPrefix}`} component={Welcome} />
+                <Route path={`${urlPrefix}catalogs`} component={Catalogs} />
+                <Route path={`${urlPrefix}start`} component={DmptStart} />
+                {/* <Route path={`${urlPrefix}:brokerSubmissionId/`} */}
+                {/*    component={SubmissionDetail}/> */}
+                {/* <Route path={`${urlPrefix}:brokerSubmissionId/`} */}
+                {/*    component={DetailBoard}/> */}
+            </Switch>
+        </RdmoContext.Provider>
+    );
+};
 
 export default App;
