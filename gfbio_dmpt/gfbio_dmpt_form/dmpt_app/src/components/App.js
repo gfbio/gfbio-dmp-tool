1d53f8fa9b26364c0f2e34d5d5c7f70de7701ddc
marc.weber01 <maweber@mpi-bremen.de>
2021-07-26 08:35:03 +0200

    2 start fetch catalogs

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/App.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import { Route, Switch } from 'react-router-dom';
 import Welcome from './Welcome';
+import Catalogs from './Catalogs';
 
 // TODO: to work when served in django template this prefix has to match
 //  the urls.py + global urls.py entry -> currently (...) regex=r'submissions/ui/', (...)
 // TODO: use this prefix when development with django (-->  url('app/', views.DmptFrontendView.as_view()))
 // eslint-disable-next-line no-unused-vars
-const urlPrefix = '/dmpt/app/';
+// const urlPrefix = '/dmpt/app/';
 
 // for updateview:
 // const urlPrefix = '/curation/submissions/form/';
 
 // TODO: use this prefix when developing with npm start
 // eslint-disable-next-line no-unused-vars
-// const urlPrefix = '/';
+const urlPrefix = '/';
 
 const App = () => (
     <Switch>
         <Route exact path={`${urlPrefix}`} component={Welcome} />
+        <Route path={`${urlPrefix}catalogs`} component={Catalogs} />
         {/* <Route path={`${urlPrefix}:brokerSubmissionId/`} */}
         {/*    component={SubmissionDetail}/> */}
         {/* <Route path={`${urlPrefix}:brokerSubmissionId/`} */}
--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/Welcome/index.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/Welcome/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // TODO: admin like filters -> dedicated views
 // TODO: pagination -> via DRF
 // TODO: column sorting. data, type etc. -> dedicated view with triggers ?
 // TODO: admin style search ? how does admin know what field to search ?
 export default function Welcome() {
-
+    console.info('Welcome render ....');
     return (
         <div>
             <h1>Welcome to GFBio DMPT</h1>
+            <Link to={{
+                pathname: 'catalogs'
+            }}>
+                Catalogs
+            </Link>
         </div>
     );
 };
