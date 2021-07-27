497e4e8e9b0346b95402be3cd375630f1815b5bb
marc.weber01 <maweber@mpi-bremen.de>
2021-07-27 21:50:59 +0200

    2 get questions prototype different way

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptStart/index.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptStart/index.js
@@ -4,13 +4,11 @@ import { API_ROOT } from '../../constants/api/api_constants';
 import RdmoContext from '../RdmoContext';
 
 function useDmptStart() {
-    const [projectResponse, setProjectResponse] = useState({});
+    // const [projectResponse, setProjectResponse] = useState({});
     const [processing, setProcessing] = useState(false);
-    const [firstSectionId, setFirstSectionId] = useState(-1);
+    // const [firstSectionId, setFirstSectionId] = useState(-1);
 
     const rdmoContext = useContext(RdmoContext);
-    console.log(rdmoContext);
-    console.log('------------------');
 
     useEffect(() => {
         async function prepareDmptStart() {
@@ -42,7 +40,48 @@ function useDmptStart() {
             // }
             // ------------------------------------------------------------
 
-            const cata
+            setProcessing(true);
+            // section for gfbio catalog id hardcoded
+            const catalogId = '18';
+
+            await axios.get(
+                `${API_ROOT}questions/sections/?catalog=${catalogId}`,
+                {
+                    headers: { 'Authorization': 'Token a801025296b509457327cac484513e62592167a8' }
+                }).then((response) => {
+                rdmoContext.assignSections(response.data);
+                return axios.get(
+                    `${API_ROOT}questions/questionsets/?section=${response.data[0].id}`,
+                    {
+                        headers: { 'Authorization': 'Token a801025296b509457327cac484513e62592167a8' }
+                    }
+                );
+            }).then((response) => {
+                rdmoContext.assignQuestionSets(response.data);
+                const questions = [];
+                response.data.forEach(qs => {
+                    axios.get(
+                        `${API_ROOT}questions/questions/?questionset=${qs.id}`,  // section for gfbio catalog id hardcoded
+                        {
+                            headers: { 'Authorization': 'Token a801025296b509457327cac484513e62592167a8' }
+                        }
+                    ).then(
+                        function(res) {
+                            // console.log(response.data);
+                            questions.push(res.data);
+                        }
+                    ).catch(function(error) {
+                        console.log(error);
+                    });
+                });
+                return questions;
+            }).then((response) => {
+                console.warn('last response/promise');
+                console.warn(response);
+                rdmoContext.assignQuestions(response);
+            }).finally(() => {
+                setProcessing(false);
+            });
 
             /*
             try {
