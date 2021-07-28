585b2142fed76b0f67c698d8a9cc18b038032509
marc.weber01 <maweber@mpi-bremen.de>
2021-07-28 09:51:28 +0200

    2 clean code, set feedback text

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptStart/index.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptStart/index.js
@@ -1,11 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
-import { map } from 'react-bootstrap/ElementChildren';
 import { API_ROOT } from '../../constants/api/api_constants';
 import RdmoContext from '../RdmoContext';
 
 const fetchQuestion = async (q) => {
-    console.log(`fetchQuestion ${q.id}`);
     return await axios.get(
         `${API_ROOT}questions/questions/?questionset=${q.id}`,
         {
@@ -19,10 +17,8 @@ const fetchQuestions = async (qsResponse) => {
 };
 
 function useDmptStart(rdmoContext) {
-    const [projectResponse, setProjectResponse] = useState({});
     const [processing, setProcessing] = useState(true);
-    const [queRes, setQueRes] = useState({});
-    // const [firstSectionId, setFirstSectionId] = useState(-1);
+    const [stage, setStage] = useState('... starting ...');
 
     console.log('useDmpStart');
     useEffect(() => {
@@ -60,6 +56,7 @@ function useDmptStart(rdmoContext) {
             const catalogId = '18';
 
             try {
+                setStage('... fetch sections ...');
                 const sectionResponse = await axios.get(
                     `${API_ROOT}questions/sections/?catalog=${catalogId}`,  // section for gfbio catalog id hardcoded
                     {
@@ -67,6 +64,7 @@ function useDmptStart(rdmoContext) {
                     }
                 );
                 rdmoContext.assignSections(sectionResponse.data);
+                setStage('... fetch questionsets ...');
                 const qsResponse = await axios.get(
                     `${API_ROOT}questions/questionsets/?section=${sectionResponse.data[0].id}`,  // section for gfbio catalog id hardcoded
                     {
@@ -74,165 +72,50 @@ function useDmptStart(rdmoContext) {
                     }
                 );
                 rdmoContext.assignQuestionSets(qsResponse.data);
+                setStage('... fetch questions ...');
                 fetchQuestions(qsResponse).then((res) => {
-                    console.log('RES');
-                    // res;
-                    // ist;
-                    // arra;
-                    // mit;
-                    // respnoses.questions;
-                    // sind in res[i].data;
-                    // hier;
-                    // kann;
-                    // jetz;
-                    // context.questiopns;
-                    // gesetzt;
-                    // werden, da;
-                    // alles;
-                    // sequenciell;
-                    // fertig;
-                    // ist;
-                    console.log(res);
-                    // rdmoContext.assignQuestions(res)
                     const tmp = [];
                     res.map((item) => {
                         tmp.push(item.data);
                         return true;
                     });
-                    console.log(tmp);
                     rdmoContext.assignQuestions(tmp);
+                    setStage('... DONE ...');
                     setProcessing(false);
                 });
 
-                // console.log(qResponse)
-                // const questions = [];
-                // qsResponse.data.forEach(qs => {
-                //     axios.get(
-                //         `${API_ROOT}questions/questions/?questionset=${qs.id}`,  // section for gfbio catalog id hardcoded
-                //         {
-                //             headers: { 'Authorization': 'Token a801025296b509457327cac484513e62592167a8' }
-                //         }
-                //     ).then(
-                //         function(response) {
-                //             // console.log(response.data);
-                //             questions.push(response.data);
-                //         }
-                //     ).catch(function(error) {
-                //         console.log(error);
-                //     });
-                // });
-                // rdmoContext.assignQuestions(questions);
             } catch (e) {
                 console.error(e);
             } finally {
-                // setProcessing(false);
+                ;
             }
-
-            // ------------------------------------------------------------
-            // FIXME: question are not waiting for finish
-            /* await axios.get(
-                `${API_ROOT}questions/sections/?catalog=${catalogId}`,
-                {
-                    headers: { 'Authorization': 'Token a801025296b509457327cac484513e62592167a8' }
-                }).then((response) => {
-                rdmoContext.assignSections(response.data);
-                return axios.get(
-                    `${API_ROOT}questions/questionsets/?section=${response.data[0].id}`,
-                    {
-                        headers: { 'Authorization': 'Token a801025296b509457327cac484513e62592167a8' }
-                    }
-                );
-            }).then((response) => {
-                rdmoContext.assignQuestionSets(response.data);
-                const questions = {};
-                response.data.forEach(qs => {
-                    axios.get(
-                        `${API_ROOT}questions/questions/?questionset=${qs.id}`,  // section for gfbio catalog id hardcoded
-                        {
-                            headers: { 'Authorization': 'Token a801025296b509457327cac484513e62592167a8' }
-                        }
-                    ).then(
-                        function(res) {
-                            console.log(`question response for ${  qs.id}`);
-                            console.log(typeof res.data);
-                            console.log(res.data);
-                            console.log('.............');
-                            // questions.push(res.data);
-                            questions[qs.id] = res.data;
-                        }
-                    ).catch(function(error) {
-                        console.log(error);
-                    });
-                });
-                rdmoContext.assignQuestions(questions);
-                setQueRes(questions);
-                return questions;
-            }).then((response) => {
-                console.warn('last response/promise');
-                console.warn(response);
-                // rdmoContext.assignQuestions(response);
-                // setQueRes(response.map(item => {
-                //     return <li>{item.id}</li>;
-                // }));
-            }).finally(() => {
-                setProcessing(false);
-            }); */
-
-            // ------------------------------------------------------------
 
         }
 
         prepareDmptStart();
     }, []);
 
-    return [projectResponse, processing, queRes];
+    return [processing, stage];
 }
 
 // eslint-disable-next-line no-unused-vars
 function DmptStart(props) {
-    // const bsi = props.bsi || '';
     const rdmoContext = useContext(RdmoContext);
-    const [projectResponse, processing, queRes] = useDmptStart(rdmoContext);
+    const [processing, stage] = useDmptStart(rdmoContext);
 
     // TODO: mehr feedback nach requests. z.B. 'getting sections', 'gettins sets', 'getting quesetions', 'preparing forms'
     console.log(`DmptStart. processing ${processing}`);
     let body = (
-        <h2><i>...processing</i></h2>
+        <div>
+            <h2><i>{stage}</i></h2>
+        </div>
     );
-    if (!processing) {
-        console.log('no processing. context: ');
-        console.debug(rdmoContext);
-        // console.log(typeof rdmoContext.questions_data);
-        // console.log(rdmoContext.questions_data.length);
-        // console.warn(queRes);
-        // let x = queRes.map((q, index) => {
-        //     console.log(q);
-        //     return <li>{q[0]}</li>;
-        // });
-        // console.info(x);
-        // let tq = rdmoContext.questions_data;
-        // const questions = tq.map((q, index) => {
-        //     console.log(`${index} ---- q:`);
-        //     return true;
-        // });
-        //     console.log(`${index} ---- q:`);
-        //     console.log(q);
-        //     if (q.size === 1) {
-        //         return <li key={index}>
-        //             <p>{q[0].id}</p>
-        //         </li>;
-        //     }
-        //     return <li>öosjkndf</li>;
-        //
-        // });
-        // console.log(questions);
-        // body = <ul>
-        //     {questions}
-        // </ul>;
-        body = (
-            <h3>Done</h3>
-        );
-    }
+    // if (!processing) {
+    //     console.log('no processing. context: ');
+    //     body = (
+    //         <h3>Done</h3>
+    //     );
+    // }
     return (
         <div>
             <h1 style={{ textTransform: 'uppercase' }}>DmptStart</h1>
