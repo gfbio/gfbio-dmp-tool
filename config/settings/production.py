90115fb6ca48933babe4ae321c8d7b2fa14c0cb2
marc.weber01 <maweber@mpi-bremen.de>
2021-08-25 22:20:53 +0200

    master fix compressor

--- config/settings/production.py
+++ config/settings/production.py
@@ -192,9 +192,10 @@ sentry_sdk.init(
 # Your stuff...
 # ------------------------------------------------------------------------------
 # TODO: for now ...
-WHITENOISE_MANIFEST_STRICT = False
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
+# WHITENOISE_MANIFEST_STRICT = False
+COMPRESS_STORAGE = 'compressor.storage.GzipCompressorFileStorage'
+COMPRESS_ENABLED = env.bool('COMPRESS_ENABLED', default=True)
+COMPRESS_OFFLINE = env.bool('COMPRESS_OFFLINE', default=True)
 
 COMPRESS_PRECOMPILERS = (
     ('text/x-sass', 'django_libsass.SassCompiler'),
