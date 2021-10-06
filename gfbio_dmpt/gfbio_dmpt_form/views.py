974423e75a19b6062a1248705c1f81e791abc920
marc.weber01 <maweber@mpi-bremen.de>
2021-10-06 14:02:21 +0200

    20 clean view code from rebase

--- gfbio_dmpt/gfbio_dmpt_form/views.py
+++ gfbio_dmpt/gfbio_dmpt_form/views.py
@@ -3,14 +3,9 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
-
-
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
-from django.views.generic.list import ListView
-from django.views.generic.edit import FormView
-# from formtools.wizard.views import SessionWizardView
+from rdmo.core.utils import render_to_format
+from rdmo.projects.models import Project
+from rdmo.projects.views import ProjectAnswersView
 
 
 class CSRFViewMixin(View):
@@ -19,75 +14,10 @@ class CSRFViewMixin(View):
     def get(self, request, *args, **kwargs):
         return super().get(self, request, *args, **kwargs)
 
+
 # React App in this template
 class DmptFrontendView(CSRFViewMixin, TemplateView):
     template_name = 'gfbio_dmpt_form/dmpt.html'
-# rdmo model import
-from rdmo.projects.models import Project
-from rdmo.projects.views import ProjectAnswersExportView
-from rdmo.projects.views import ProjectAnswersView
-
-from rdmo.core.utils import render_to_format
-
-# def dmpt_form_view(request):
-#     if request.method == 'POST':
-#         form = GFBioDmptForm(request.POST)
-#         if form.is_valid():
-#             return HttpResponseRedirect('/thanks/')
-#
-#     else:
-#         form = GFBioDmptForm()
-#
-#     return render(request, 'gfbio_dmpt_form/dmpt_form.html', {'form': form})
-
-
-# class DmptFormView(FormView):
-#     template_name = 'gfbio_dmpt_form/dmpt_form.html'
-#     form_class = GFBioDmptForm
-#     success_url = '/thanks/'
-#
-#     # TODO: new tmp project on blank form
-#     # TODO: check for existing temp project
-#     # TODO: delete old temp projects
-#     def get(self, request, *args, **kwargs):
-#         print('CUSTOM_GET')
-#         return super().get(self, request, *args, **kwargs)
-#
-#     def form_valid(self, form):
-#         # This method is called when valid form data has been POSTed.
-#         # It should return an HttpResponse.
-#
-#         # form.send_email()
-#         return super().form_valid(form)
-
-
-# https://django-formtools.readthedocs.io/en/latest/wizard.html#wizard-template-for-each-form
-# class ContactWizard(SessionWizardView):
-#     form_list = [ContactForm1, ContactForm2, ContactForm3, ContactForm4]
-#
-#     def done(self, form_list, **kwargs):
-#         # do_something_with_the_form_data(form_list)
-#         print('CONTACT WIZARD FORM LIST', form_list)
-#         # return HttpResponseRedirect('/page-to-redirect-to-when-done/')
-#         return render(self.request, 'done.html', {
-#             'form_data': [form.cleaned_data for form in form_list],
-#         })
-#
-#
-# class DmptFormWizardView(SessionWizardView):
-#     form_list = [GeneralInformationForm, DataCollectionForm,
-#                  DocumentationAndMetadataForm, EthnicsAndLegalComplianceForm,
-#                  PreservationAndSharingForm]
-#
-#     template_name = 'gfbio_dmpt_form/dmp_wizard.html'
-#
-#     def done(self, form_list, **kwargs):
-#         # do_something_with_the_form_data(form_list)
-#         print('DmptFormWizardView FORM LIST', form_list)
-#         return HttpResponseRedirect('/page-to-redirect-to-when-done/')
-#         # return render(self.request, 'done.html', {
-#         #     'form_data': [form.cleaned_data for form in form_list],
-#         # })
 
 
 # React App in this template
@@ -101,8 +31,6 @@ class DmpExportView(ProjectAnswersView):
     template_name = "gfbio_dmpt_export/dmp_export.html"
 
     def render_to_response(self, context, **response_kwargs):
-        #return render_to_format(self.request, context['format'], context['title'], 'projects/project_view_export.html', context)
-        return render_to_format(self.request, 'pdf', 'title', 'gfbio_dmpt_export/dmp_export.html', context)
-
-
-
+        # return render_to_format(self.request, context['format'], context['title'], 'projects/project_view_export.html', context)
+        return render_to_format(self.request, 'pdf', 'title',
+                                'gfbio_dmpt_export/dmp_export.html', context)
