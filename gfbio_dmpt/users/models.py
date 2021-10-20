b032f3bf1e261854d3c9adcc50af9df6d54cb8fb
cpfaff <ctpfaff@arcor.de>
2021-10-20 09:26:02 +0200

    Fixing import

--- gfbio_dmpt/users/models.py
+++ gfbio_dmpt/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django import models
+from django.db import models
 from django.db import IntegrityError
 from django.utils.timezone import now
 
