5577845d12e0ad91fe830203f8b57133046372cb
marc.weber01 <maweber@mpi-bremen.de>
2022-01-21 11:30:11 +0100

    54 add tests for DmptProject model

--- gfbio_dmpt/gfbio_dmpt_form/tests/test_models.py
+++ gfbio_dmpt/gfbio_dmpt_form/tests/test_models.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
-import json
-from pprint import pp
 
 from django.test import TestCase
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient
 from rdmo.projects.models import Project
+
+from gfbio_dmpt.gfbio_dmpt_form.models import DmptProject
 from gfbio_dmpt.users.models import User
 
 
 class TestDmptProject(TestCase):
 
-    def test_unrelated_instance(self):
-        pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.std_user = User.objects.create_user(
+            username="john",
+            email="john@doe.de",
+            password="secret",
+            is_staff=False,
+            is_superuser=False,
+        )
+
+    def test_instance(self):
+        rp = Project.objects.create()
+        dmpt_project = DmptProject.objects.create(
+            rdmo_project=rp, user=self.std_user)
+        self.assertEqual(self.std_user.id, dmpt_project.user.id)
+        self.assertEqual(rp.pk, dmpt_project.rdmo_project.pk)
+
+    def test_str(self):
+        rp = Project.objects.create(title='foo_bar')
+        dmpt_project = DmptProject.objects.create(
+            rdmo_project=rp, user=self.std_user)
+        self.assertEqual('foo_bar', dmpt_project.__str__())
