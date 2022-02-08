84933ce33eacd277d73ef9e13e57f35dfc361b26
cpfaff <ctpfaff@arcor.de>
2022-02-08 12:50:08 +0100

    Updates the template for dmp export
    * This layout now better handles long titles

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/Summary/index.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/Summary/index.js
@@ -6,12 +6,12 @@ import PropTypes from 'prop-types';
 import ScrollToTop from '../ScrollToTop';
 import {
     PROJECT_API_ROOT,
-    URL_PREFIX
+    URL_PREFIX,
 } from '../../constants/api/api_constants';
 import RdmoContext from '../RdmoContext';
 import {
     checkBackendParameters,
-    resetContext
+    resetContext,
 } from '../../utils/backend_context';
 
 // FIXME: refactor move to general module
@@ -23,8 +23,10 @@ function getCookie(name) {
         for (let i = 0; i < cookies.length; i += 1) {
             const cookie = cookies[i].trim();
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            if (cookie.substring(0, name.length + 1) === `${name}=`) {
+                cookieValue = decodeURIComponent(
+                    cookie.substring(name.length + 1)
+                );
                 break;
             }
         }
@@ -35,13 +37,19 @@ function getCookie(name) {
 const saveProject = async (token, userId, projectId) => {
     try {
         const csrftoken = getCookie('csrftoken');
-        const response = await axios.post(`${PROJECT_API_ROOT}dmptprojects/`, {
-            'rdmo_project': projectId, 'user': userId
-        }, {
-            headers: {
-                'Authorization': `Token ${token}`, 'X-CSRFToken': csrftoken
+        const response = await axios.post(
+            `${PROJECT_API_ROOT}dmptprojects/`,
+            {
+                rdmo_project: projectId,
+                user: userId,
+            },
+            {
+                headers: {
+                    Authorization: `Token ${token}`,
+                    'X-CSRFToken': csrftoken,
+                },
             }
-        });
+        );
         return response;
     } catch (e) {
         console.error(e);
@@ -79,7 +87,8 @@ function Summary() {
             saveProject(
                 rdmoContext.backend_context.token,
                 rdmoContext.backend_context.user_id,
-                projectId).then((result) => {
+                projectId
+            ).then((result) => {
                 // console.log('saveProject handler. result');
                 // console.log(result);
                 rdmoContext.assignDmptProjectId(result.data.id);
@@ -92,36 +101,31 @@ function Summary() {
     };
 
     let saveSection = (
-        <Col lg={6} className='p-3'>
-            <i className='mdi mdi-content-save-edit-outline' />
-            <h6>
-                Save Data Management Plan
-            </h6>
-            <div className='d-grid gap-2'>
+        <Col lg={6} className="p-3">
+            <i className="mdi mdi-content-save-edit-outline" />
+            <h6>Save Data Management Plan</h6>
+            <div className="d-grid gap-2">
                 <Button
-                    className='btn btn-secondary btn-green'
+                    className="btn btn-secondary btn-green"
                     onClick={saveProjectHandler}
-                >Save
+                >
+                    Save
                 </Button>
             </div>
         </Col>
     );
     if (saving) {
         saveSection = (
-            <Col lg={6} className='p-3'>
-                <i className='mdi mdi-content-save-edit-outline' />
-                <h6>
-                    ... Saving Data Management Plan ...
-                </h6>
+            <Col lg={6} className="p-3">
+                <i className="mdi mdi-content-save-edit-outline" />
+                <h6>... Saving Data Management Plan ...</h6>
             </Col>
         );
     } else if (savingDone) {
         saveSection = (
-            <Col lg={6} className='p-3'>
-                <i className='mdi mdi-content-save-edit-outline' />
-                <h6>
-                    Saving completed successfully !
-                </h6>
+            <Col lg={6} className="p-3">
+                <i className="mdi mdi-content-save-edit-outline" />
+                <h6>Saving completed successfully !</h6>
             </Col>
         );
     }
@@ -134,32 +138,28 @@ function Summary() {
         setDiscardingDone(true);
     };
     let discardSection = (
-        <Col lg={6} className='p-3'>
-            <i className='mdi mdi-location-exit' />
-            <h6>
-                Finish
-            </h6>
-            <div className='d-grid gap-2'>
-                <Button className='btn btn-secondary btn-green'
-                        onClick={discardProjectHandler}>Discard
-                    &
-                    Exit
+        <Col lg={6} className="p-3">
+            <i className="mdi mdi-location-exit" />
+            <h6>Finish</h6>
+            <div className="d-grid gap-2">
+                <Button
+                    className="btn btn-secondary btn-green"
+                    onClick={discardProjectHandler}
+                >
+                    Discard & Exit
                 </Button>
             </div>
         </Col>
     );
     if (discarding) {
         discardSection = (
-            <Col lg={6} className='p-3'>
-                <i className='mdi mdi-location-exit' />
-                <h6>
-                    ... Deleting data & prepare to exit ...
-                </h6>
+            <Col lg={6} className="p-3">
+                <i className="mdi mdi-location-exit" />
+                <h6>... Deleting data & prepare to exit ...</h6>
             </Col>
         );
     } else if (discardingDone) {
-        return <Redirect push
-                         to={`${URL_PREFIX}`} />;
+        return <Redirect push to={`${URL_PREFIX}`} />;
     }
 
     // const downloadPdfHandler = (id) => {
@@ -171,16 +171,16 @@ function Summary() {
     //     setDownloadDone(true);
     // };
     const downloadPdfSection = (
-        <Col lg={6} className='p-3'>
-            <i className='mdi mdi-download-circle-outline' />
-            <h6>
-                Dowload PDF file
-            </h6>
-            <div className='d-grid gap-2'>
-                <a href={`${PROJECT_API_ROOT}export/${projectId}/pdf/`}
-                    className='btn btn-secondary btn-green'
+        <Col lg={6} className="p-3">
+            <i className="mdi mdi-download-circle-outline" />
+            <h6>Dowload PDF file</h6>
+            <div className="d-grid gap-2">
+                <a
+                    href={`${PROJECT_API_ROOT}export/${projectId}/pdf/`}
+                    className="btn btn-secondary btn-green"
                     // onClick={downloadPdfHandler}
-                >Download
+                >
+                    Download
                 </a>
             </div>
         </Col>
@@ -203,46 +203,45 @@ function Summary() {
     //     downloadPdfSection = <a href={`${PROJECT_API_ROOT}export/${projectId}/pdf/`}>DOWNLOAD ?</a>;
     // }
 
-    return (<div id='summary' className='text-center'>
-        <ScrollToTop />
+    return (
+        <div id="summary" className="text-center">
+            <ScrollToTop />
 
-        <Row>
-            <div className='col-12'>
-                <h3>Summary</h3>
-            </div>
-        </Row>
-
-        <Row className='mt-3'>
-            <Col lg={12}>
-                <h5>Send a DMP support request to GFBio, download your DMP
-                    or save it to your personal account</h5>
-            </Col>
-        </Row>
-
-        <Row className='mt-5'>
-
-            <Col lg={6} className='p-3'>
-                <i className='mdi mdi-email-send-outline' />
-                <h6>
-                    Request Data Management Plan Support
-                </h6>
-                <div className='d-grid gap-2'>
-                    <Button className='btn btn-secondary btn-green'>Send
-                        Request
-                    </Button>
+            <Row>
+                <div className="col-12">
+                    <h3>Summary</h3>
                 </div>
-            </Col>
+            </Row>
 
-            {downloadPdfSection}
+            <Row className="mt-3">
+                <Col lg={12}>
+                    <h5>
+                        Send a DMP support request to GFBio, download your DMP
+                        or save it to your personal account
+                    </h5>
+                </Col>
+            </Row>
 
-        </Row>
+            <Row className="mt-5">
+                <Col lg={6} className="p-3">
+                    <i className="mdi mdi-email-send-outline" />
+                    <h6>Request Data Management Plan Support</h6>
+                    <div className="d-grid gap-2">
+                        <Button className="btn btn-secondary btn-green">
+                            Send Request
+                        </Button>
+                    </div>
+                </Col>
 
-        <Row className='mt-3'>
-            {saveSection}
-            {discardSection}
-        </Row>
+                {downloadPdfSection}
+            </Row>
 
-    </div>);
+            <Row className="mt-3">
+                {saveSection}
+                {discardSection}
+            </Row>
+        </div>
+    );
 }
 
 // Summary.defaultProps = {
