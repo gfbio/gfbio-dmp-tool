149cee59cde068caa060d30bdbf4da0f76874ecb
marc.weber01 <marc.weber01@gmail.com>
2022-02-10 21:27:43 +0100

    25 replace old MPI email with e.v. email

--- config/settings/base.py
+++ config/settings/base.py
@@ -274,7 +274,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Marc Weber""", "maweber@mpi-bremen.de")]
+ADMINS = [("""Marc Weber""", "mweber@gfbio.org")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
