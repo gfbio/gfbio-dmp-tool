f060c2f81c35b6588a15414d19a8a862e2ab83aa
marc.weber01 <marc.weber01@gmail.com>
2022-03-22 21:41:16 +0100

    84 mark mandatory textarea fields with asterisk and text

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/FormTextArea/index.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/FormTextArea/index.js
@@ -8,20 +8,50 @@ function FormTextArea(props) {
     const { item, value, handleChange } = props;
     const rdmoContext = useContext(RdmoContext);
     const val = formFieldInit(value, rdmoContext, item);
+
+    // TODO: alternatively add a icon (or keep the asterisk) and add a small tooltip stating "mandatory field".
+    let headerText = (<h5>{item.text_en} <b>*</b></h5>);
+
+    let helpText = '(This field is mandatory)';
+    if (item.help_en) {
+        helpText = `${item.help_en} (This field is mandatory)`;
+    }
+
+    let inputField = (<textarea
+        name={item.key}
+        id={item.key}
+        className='form-control'
+        rows='3'
+        onChange={(e) => handleChange(e, item)}
+        required
+    >
+        {val}
+    </textarea>);
+
+    if (item.is_optional) {
+        headerText = (<h5>{item.text_en}</h5>);
+        helpText = item.help_en;
+        inputField = (<textarea
+            name={item.key}
+            id={item.key}
+            className='form-control'
+            rows='3'
+            onChange={(e) => handleChange(e, item)}
+        >
+            {val}
+        </textarea>);
+    }
+
     return (
         <div className='form-group' key={item.id}>
             <label htmlFor={item.key}>
-                <h5>{item.text_en}</h5>
+                {headerText}
                 <small id={`help_${item.key}`}
                     className='form-text text-muted'>
-                    {item.help_en}
+                    {helpText}
                 </small>
             </label>
-            <textarea name={item.key} id={item.key} className='form-control'
-                rows='3' onChange={(e) => handleChange(e, item)}>
-                {val}
-            </textarea>
-
+            {inputField}
         </div>
     );
 }
