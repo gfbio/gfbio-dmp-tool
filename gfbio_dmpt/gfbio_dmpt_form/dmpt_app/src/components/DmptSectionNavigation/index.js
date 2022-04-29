cab995f79b2f991f79899d2ac37b8213472eec9f
Marc Weber <mweber@gfbio.org>
2022-04-29 14:45:47 +0200

    86 setup stick sidebar for section
    - add react-stickynode to package.json
    - code formatting

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptSectionNavigation/index.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptSectionNavigation/index.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
-import {SECTIONS_ROOT} from "../../constants/api/api_constants";
+import Sticky from "react-stickynode";
+import { SECTIONS_ROOT } from "../../constants/api/api_constants";
 import DmptLoading from "../DmptLoading";
 import DmptSection from "../DmptSection";
 import useDmptSectionForm from "../DmptHooks/formHooks";
@@ -13,8 +14,8 @@ const useDmptSectionNavigation = (catalogId, token) => {
         async function prepareDmptSectionList() {
             try {
                 const result = await axios.get(`${SECTIONS_ROOT}${catalogId}/`, {
-                    headers: {Authorization: `Token ${token}`},
-                },);
+                    headers: { Authorization: `Token ${token}` }
+                });
                 setSectionList(result.data);
                 setProcessing(false);
             } catch (error) {
@@ -47,7 +48,7 @@ const sectionsAsListElements = (sectionList, sectionIndex, handleClick) => {
     const maxLength = 25;
     return sectionList.map((section, index) => {
 
-        let {title} = section;
+        let { title } = section;
         if (title.length > maxLength) {
             title = `${section.title.substring(0, maxLength)}...`;
         }
@@ -67,16 +68,16 @@ const sectionsAsListElements = (sectionList, sectionIndex, handleClick) => {
 };
 
 const fakeSubmit = () => {
-    console.log('DmptSectionNavigation | fakeSubmit | inputs: ', inputs);
+    console.log("DmptSectionNavigation | fakeSubmit | inputs: ", inputs);
 };
 
 function DmptSectionNavigation(props) {
-    const {catalogId, token} = props;
+    const { catalogId, token } = props;
 
     const [processing, sectionList] = useDmptSectionNavigation(catalogId, token);
     const [sectionIndex, setSectionIndex] = useState(0);
 
-    const {inputs, handleInputChange, handleSubmit} = useDmptSectionForm(fakeSubmit);
+    const { inputs, handleInputChange, handleSubmit } = useDmptSectionForm(fakeSubmit);
 
     const sections = sectionsAsListElements(sectionList, sectionIndex, setSectionIndex);
     const sectionsLength = sectionList.length;
@@ -85,7 +86,7 @@ function DmptSectionNavigation(props) {
 
     if (processing) {
         return (
-            <DmptLoading/>
+            <DmptLoading />
         );
     }
 
@@ -99,35 +100,63 @@ function DmptSectionNavigation(props) {
                     </ul>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12">
-                    <DmptSection token={token} catalogId={catalogId} sectionIndex={sectionIndex}
-                        handleInputChange={handleInputChange} handleSubmit={handleSubmit}/>
+
+            <div className="row" id="section-wrapper-row">
+
+                <div className="col-3" id="section-sub-navi">
+                    <Sticky top={80}>
+                        <div className="list-group">
+                            <button type="button" className="list-group-item list-group-item-action active"
+                                aria-current="true">
+                                The current button
+                            </button>
+                            <button type="button" className="list-group-item list-group-item-action">A second item
+                            </button>
+                            <button type="button" className="list-group-item list-group-item-action">A third button
+                                item
+                            </button>
+                            <button type="button" className="list-group-item list-group-item-action">A fourth button
+                                item
+                            </button>
+                            <button type="button" className="list-group-item list-group-item-action" disabled>A disabled
+                                button item
+                            </button>
+                        </div>
+                    </Sticky>
+                </div>
+
+                <div className="col-9" id="section-content">
+                    <div className="row">
+                        <div className="col-12">
+                            <DmptSection token={token} catalogId={catalogId} sectionIndex={sectionIndex}
+                                handleInputChange={handleInputChange} handleSubmit={handleSubmit} />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6 ps-4">
+                            <button className="btn btn-secondary btn-green"
+                                type="button" onClick={() => backHandler(sectionIndex, setSectionIndex)}
+                            >Back
+                            </button>
+                        </div>
+                        <div className="col-6 pe-4">
+                            <button
+                                className="btn btn-secondary btn-green float-end"
+                                type="button"
+                                onClick={() => continueHandler(sectionIndex, sectionsLength, setSectionIndex)}
+                            >Continue
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-6 ps-4">
-                    <button className="btn btn-secondary btn-green"
-                        type="button"
-                        onClick={() => backHandler(sectionIndex, setSectionIndex)}
-                    >Back
-                    </button>
-                </div>
-                <div className="col-6 pe-4">
-                    <button
-                        className="btn btn-secondary btn-green float-end"
-                        type="button"
-                        onClick={() => continueHandler(sectionIndex, sectionsLength, setSectionIndex)}
-                    >Continue
-                    </button>
-                </div>
-            </div>
+
         </div>
     );
 }
 
 DmptSectionNavigation.propTypes = {
-    token: PropTypes.string.isRequired, catalogId: PropTypes.number.isRequired,
+    token: PropTypes.string.isRequired, catalogId: PropTypes.number.isRequired
 };
 
 export default DmptSectionNavigation;
