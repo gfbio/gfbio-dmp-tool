6e7906505bc5982fb3f13c5ca4151ac134e38005
cpfaff <ctpfaff@arcor.de>
2022-05-05 12:39:31 +0200

    Removing validator from text field for now

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptFormFields/textarea.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptFormFields/textarea.js
@@ -1,20 +1,8 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import validator from 'validator';
 
 function TextArea(props) {
     const { question, handleChange, initialValue } = props;
-    const [emailError, setEmailError] = useState('');
-
-    const validateEmail = (event) => {
-        const email = event.target.value;
-
-        if (validator.isEmail(email)) {
-            setEmailError('Valid Email :)');
-        } else {
-            setEmailError('Enter valid Email!');
-        }
-    };
 
     // { question.is_optional ? "" : "required"}
     return (
