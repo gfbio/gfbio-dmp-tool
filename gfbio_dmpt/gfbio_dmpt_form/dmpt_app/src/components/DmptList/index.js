6f7947c29d2ec767f626b1f2d344ce86e6081505
Marc Weber <mweber@gfbio.org>
2022-05-18 15:49:21 +0200

    144 remove comments

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptList/index.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/DmptList/index.js
@@ -32,13 +32,6 @@ const useDmpList = (token) => {
     return [processing, dmpList];
 };
 
-// [
-//     {
-//         "id": 29,  --> DMPTProject id/pk
-//         "user": 1,
-//         "rdmo_project": 156, rdmo_project id/pk
-//         "title": "tmp_6IHLMd4G9wpesxX7s6ZA3"
-//     },
 const dmpsAsListElements = (dmpList) => {
     return dmpList.map((dmp, index) => {
         return (
@@ -64,18 +57,8 @@ function DmptList(props) {
         return <DmptLoading />;
     }
 
-    // let message = <></>;
-    // if (updateStatusCode >= 200 && updateStatusCode < 300) {
-    //     message = <div className="alert alert-success alert-dismissible fade show" role="alert">
-    //         <h4 className="alert-heading">Update Successful !</h4>
-    //         <p>Your Data Management Plan has been successfully updated.</p>
-    //         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
-    //     </div>;
-    // }
-
     return (
         <>
-            {/* {message} */}
             <div id="dmp-list">
                 <div className="row">
                     <div className="col-12">
@@ -88,13 +71,8 @@ function DmptList(props) {
     );
 }
 
-// DmptList.defaultProps = {
-//     updateStatusCode: 0,
-// };
-
 DmptList.propTypes = {
     token: PropTypes.string.isRequired,
-    // updateStatusCode: PropTypes.number,
 };
 
 export default DmptList;
