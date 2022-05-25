121eb18dcdf053d5174a651f37de273e9aa67734
Marc Weber <mweber@gfbio.org>
2022-05-25 15:48:28 +0200

    132 add admin and inline admin

--- gfbio_dmpt/gfbio_dmpt_form/admin.py
+++ gfbio_dmpt/gfbio_dmpt_form/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 
 from gfbio_dmpt.gfbio_dmpt_form.models import DmptProject, DmptIssue
 
-admin.site.register(DmptProject)
+
+class DmptIssueInlineAdmin(admin.TabularInline):
+    model = DmptIssue
+
+
+class DmptProjectAdmin(admin.ModelAdmin):
+    inlines = [DmptIssueInlineAdmin, ]
+
+
+admin.site.register(DmptProject, DmptProjectAdmin)
 admin.site.register(DmptIssue)
