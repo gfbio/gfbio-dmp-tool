d97812bc764b3b33b9ad35f06695aee2965ca668
cpfaff <ctpfaff@arcor.de>
2022-06-24 09:50:05 +0200

    Changes link text and adds paths (usability)
    * Navigation bar change Create DMP to My DMP's
    * Conditionally display navigation bar My DMP's (only logged in)
    * Adds path to dmp/create
    * Use new path on button in the landing page

--- gfbio_dmpt/gfbio_dmpt_form/urls.py
+++ gfbio_dmpt/gfbio_dmpt_form/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = "gfbio_dmpt_form"
 urlpatterns = [
     url("create/", views.DmptFrontendView.as_view(), name="create_dmp"),
-    # url("create/start", views.DmptFrontendView.as_view(), name="create_dmp_start"),
+    url("create/new", views.DmptFrontendView.as_view(), name="create_dmp_new"),
     path("dmptprojects/", views.DmptProjectListView.as_view(), name="dmpt_projects"),
     path(
         "dmptprojects/<int:pk>/",
@@ -18,9 +18,20 @@ urlpatterns = [
         "export/<int:pk>/<str:format>", views.DmpExportView.as_view(), name="dmp-detail"
     ),
     path("support/", views.DmptSupportView.as_view(), name="dmpt_support"),
-    path("sections/<int:catalog_id>/", views.DmptSectionListView.as_view(), name="dmpt_sections"),
-    path("section/<int:catalog_id>/<int:section_index>/", views.DmptSectionDetailView.as_view(), name="dmpt_section_detail"),
-
+    path(
+        "sections/<int:catalog_id>/",
+        views.DmptSectionListView.as_view(),
+        name="dmpt_sections",
+    ),
+    path(
+        "section/<int:catalog_id>/<int:section_index>/",
+        views.DmptSectionDetailView.as_view(),
+        name="dmpt_section_detail",
+    ),
     path("projects/", views.RdmoProjectCreateView.as_view(), name="dmpt_rdmo_projects"),
-    path("projects/values/", views.DmptRdmoProjectCreateView.as_view(), name="dmpt_rdmo_values"),
+    path(
+        "projects/values/",
+        views.DmptRdmoProjectCreateView.as_view(),
+        name="dmpt_rdmo_values",
+    ),
 ]
