404e2ce8e9cac0e9bd1d1a12578357dca00e8c8c
ctpfaff <Claas-Thido.Pfaff@gfbio.org>
2024-06-18 10:47:51 +0200

    Adds dmpt catalog model
    * This allows storing catalogs used by our dmpt
    * Enabling them makes them used as default for each new dmpt
    * Admin area allows adding and setting catalogs as active
    * Only one catalog can be activated which is ensured by save logic

--- gfbio_dmpt/gfbio_dmpt_form/models.py
+++ gfbio_dmpt/gfbio_dmpt_form/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 from rdmo.projects.models import Project
+from rdmo.questions.models.catalog import Catalog
 
 from config.settings.base import AUTH_USER_MODEL
 from gfbio_dmpt.gfbio_dmpt_form.managers import DmptProjectManager
@@ -25,3 +26,19 @@ class DmptIssue(TimeStampedModel):
 
     def __str__(self):
         return '{0}'.format(self.issue_key)
+
+
+class DmptCatalog(models.Model):
+    catalog = models.OneToOneField(Catalog, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.active:
+            DmptCatalog.objects.filter(active=True).update(active=False)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.catalog.key
+
+    class Meta:
+        app_label = 'gfbio_dmpt_form'
