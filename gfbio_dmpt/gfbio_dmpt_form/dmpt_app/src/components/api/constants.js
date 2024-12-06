eac92bedbb11a6a6057fd24800812bf96f5fb3fc
Marc Weber <mweber@gfbio.org>
2024-12-06 13:58:56 +0100

    DASS-2323 set API root to dev setting
    - update js build

--- gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/api/constants.js
+++ gfbio_dmpt/gfbio_dmpt_form/dmpt_app/src/components/api/constants.js
@@ -1,8 +1,8 @@
 // TODO: for production system
-export const API_ROOT = '/api/v1/';
+// export const API_ROOT = '/api/v1/';
 
 // TODO: for local/ development
-// export const API_ROOT = 'http://0.0.0.0:8000/api/v1/';
+export const API_ROOT = 'http://0.0.0.0:8000/api/v1/';
 
 export const PROJECT_API_ROOT = '/dmp/';
 
